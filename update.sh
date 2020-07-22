90cbb576dd96e49333cbc3e646bd07a19cac354b
ChillerDragon <chillerdragon@gmail.com>
2020-07-22 23:29:24 +0200

    Add mirror script

---
+++ update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ ! -d .git ]
+then
+    echo "Error: Make sure you are at the root of the git repo"
+    exit 1
+fi
+if ! git remote -v | grep -q irc
+then
+    echo "Error: This is not a irc git repository"
+    exit 1
+fi
+
+function cleanup() {
+    if [ -d tmp/ ]
+    then
+        rm -rf tmp/
+    fi
+}
+
+cleanup
+mkdir -p tmp/ || exit 1
+cd tmp/ || exit 1
+
+wget -r -np -nH --cut-dirs=1 -R index.html https://ddnet.tw/irclogs/ || exit 1
+mv *.log ../ddnet
+mv teeworlds/*.log ../teeworlds
+
+cleanup
+echo "done."
+
