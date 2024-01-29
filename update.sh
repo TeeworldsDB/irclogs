bde029a228ac6238d7ff6d1e4127b628ac2b7092
ChillerDragon <ChillerDragon@gmail.com>
2024-01-29 08:39:41 +0800

    Do partial download by default

--- update.sh
+++ update.sh
@@ -11,6 +11,13 @@ then
     exit 1
 fi
 
+log() {
+	echo "[*] $1"
+}
+err() {
+	echo "[-] $1" 1>&2
+}
+
 function cleanup() {
     if [ -d tmp/ ]
     then
@@ -26,9 +33,69 @@ cd tmp/ || exit 1
 
 git pull
 
-wget -r -np -nH --cut-dirs=1 -R index.html https://ddnet.org/irclogs/ || exit 1
-mv *.log ../ddnet
-mv teeworlds/*.log ../teeworlds
+download_full() {
+	log "doing full download updating all logfiles ..."
+	wget -r -np -nH --cut-dirs=1 -R index.html https://ddnet.org/irclogs/ || exit 1
+	mv ./*.log ../ddnet
+	mv teeworlds/*.log ../teeworlds
+}
+
+download_partial_newest() {
+	# this only includes ddnet not the teeworlds/ subdirectory
+	# this only redownloads logs from 2024 and later
+	local from_year=2024
+	local max_year_last_digit
+	max_year_last_digit="$((from_year - 1))"
+	max_year_last_digit="${max_year_last_digit:3}"
+	log "doing partial download starting from year $from_year ..."
+	curl -L https://ddnet.org/irclogs > index.html || exit 1
+	if [ "$from_year" -gt 2029 ]
+	then
+		err "Error: this half a decade old code refuses to work"
+		exit 1
+	fi
+	local logfile
+	while read -r logfile
+	do
+		local url
+		url="https://ddnet.org/irclogs/$logfile"
+		if ! curl -O "$url"
+		then
+			err "Error: curl failed to download $url"
+			exit 1
+		fi
+	done < <(grep -Eo '<a href="[^"]+"' index.html |
+		cut -d'"' -f2 |
+		grep '.log$' |
+		sort |
+		tac |
+		grep -vE '^201' |
+		grep -vE "^202[0-$max_year_last_digit]")
+	# TODO: this will break in 2030
+}
+
+arg_full=0
+
+for arg in "$@"
+do
+	if [ "$arg" == "--help" ] || [ "$arg" == "-h" ]
+	then
+		echo "usage: $0 [--full]"
+	elif [ "$arg" == "--full" ]
+	then
+		arg_full=1
+	else
+		echo "unknown arg '$arg'"
+		exit 1
+	fi
+done
+
+if [ "$arg_full" == "1" ]
+then
+	download_full
+else
+	download_partial_newest
+fi
 
 cleanup
 echo "done."
